db9bd1fd07ac71b6da887cde38896b78b0c7b253
Seasawher <tellagoodstory@icloud.com>
2023-10-02 19:43:45 +0900

    mathlib4 のバージョンを固定する

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,7 @@ package «lean-quiz» where
   -- add any package configuration options here
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "80b890f7e8113c7dd0832d09f8caf66cf169dcd3"
 
 @[default_target]
 lean_lib «LeanQuiz» where
