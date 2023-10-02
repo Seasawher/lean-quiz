811855984280beaaf2d99605038b67adade51562
Seasawher <tellagoodstory@icloud.com>
2023-10-02 19:44:59 +0900

    ファイル名変更

--- LeanQuiz.lean
+++ LeanQuiz.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `LeanQuiz` library.
 -- Import modules here that should be built as part of the library.
-import «LeanQuiz».Basic
+import «LeanQuiz».RemoveOne
